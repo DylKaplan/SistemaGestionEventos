9275b07ecd2e1d10d28a0b094dd3e5fa0340a1b8
serpalucas02 <serpalucas02@gmail.com>
2024-06-29 12:42:04 -0300

    Fix codigo

---
+++ SistemaGestionEventos/Migrations/20240629151427_CrearDB.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaGestionEventos.Migrations
+{
+    /// <inheritdoc />
+    public partial class CrearDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
